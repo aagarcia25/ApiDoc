68fdcd4c4be2bf4d65b7e780e22a4ad32a3aee24
aagarcia25 <109979605+aagarcia25@users.noreply.github.com>
2023-04-19 13:56:11 -0600

    Update JwtMiddlware.php

--- app/Http/Middleware/JwtMiddlware.php
+++ app/Http/Middleware/JwtMiddlware.php
@@ -21,54 +21,54 @@ class JwtMiddlware
     public function handle(Request $request, Closure $next)
     {
 
-      
+
             // attempt to verify the credentials and create a token for the user
             $token = $request->header('Authorization');
             if($token){
                 $decoded = (json_decode(base64_decode(str_replace('_', '/', str_replace('-','+',explode('.', $token)[1])))));
                 $decoded_array = (array) $decoded;
-                $nombreUsuario= ($decoded_array['NombreUsuario']);
-                $idusuario=     (trim($decoded_array['IdUsuario']));
+                //$nombreUsuario= ($decoded_array['NombreUsuario']);
+               // $idusuario=     (trim($decoded_array['IdUsuario']));
                 $iat=           ($decoded_array['iat']);
                 $exp =          ($decoded_array['exp']);
                 $date = time();
                 $f1=date("Y-m-d (H:i:s)", $date);
                 $f2=date("Y-m-d (H:i:s)", $exp);
                 $tokenvalido=$f1 > $f2;
-               
+
                 if(!$tokenvalido){
-                    if($this->checkUser($idusuario)){
+                 //   if($this->checkUser($idusuario)){
                         return $next($request);
-                    }else{
-                        return response()->json(['status' => 'Usuario no Existe'], 401);
-                    }
-                   
+                 //   }else{
+                 //       return response()->json(['status' => 'Usuario no Existe'], 401);
+                 //   }
+
                 }else{
                     return response()->json(['status' => 'Token Expirado'], 401);
                 }
-    
-    
+
+
     /*
                 print_r($decoded_array['NombreUsuario']);
                 print_r($decoded_array['IdUsuario']);
                 print_r($decoded_array['iat']);
                 print_r($decoded_array['exp']);*/
-    
-    
+
+
                //  $TokenValido= ((( number_format($decoded_array['exp'])  - (Carbon::now() / 1000)) / 60)) ;
-              
+
 
 
 
             }else{
                 return response()->json(['status' => 'Authorization Token not found'], 401);
             }
-           
-
-            
 
 
-        
+
+
+
+
     }
 
     public function checkUser($id){
@@ -80,7 +80,7 @@ class JwtMiddlware
         } catch (\Exception $e) {
             $response = false;
         }
-      
+
         return  $response;
      }
 
