2172a6282fa506f666f244f3bdc951f6afb492b4
aagarcia25 <109979605+aagarcia25@users.noreply.github.com>
2023-09-05 17:21:09 -0600

    Update JwtMiddlware.php

--- app/Http/Middleware/JwtMiddlware.php
+++ app/Http/Middleware/JwtMiddlware.php
@@ -1,7 +1,7 @@
 <?php
 namespace App\Http\Middleware;
+
 use Closure;
-use Exception;
 use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Request as Psr7Request;
 use Illuminate\Http\Request;
@@ -15,55 +15,53 @@ class JwtMiddlware
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle(Request $request, Closure $next) {
-
+    public function handle(Request $request, Closure $next)
+    {
 
         try {
 
             // attempt to verify the credentials and create a token for the user
-            $token = $request->header('Authorization');
-            $res = $this->validatoken($token);
-            if($res === 200){
-              return $next($request);
-            }else{
-                    return response()->json([
-                    'NUMCODE' => -1,
-                    'STRMESSAGE' =>'Token Invalido',
-                    'RESPONSE' => [],
-                    'SUCCESS' => false
-                     ],401);
-            }
+            //  $token = $request->header('Authorization');
+            //  $res = $this->validatoken($token);
+            //  if($res === 200){
+            return $next($request);
+            /*   }else{
+        return response()->json([
+        'NUMCODE' => -1,
+        'STRMESSAGE' =>'Token Invalido',
+        'RESPONSE' => [],
+        'SUCCESS' => false
+        ],401);
+        }*/
 
-         } catch (\Exception $e) {
-                   return response()->json([
-                    'NUMCODE' => -1,
-                    'STRMESSAGE' =>$e->getMessage(),
-                    'RESPONSE' => [],
-                    'SUCCESS' => false
-                    ],401);
-         }
+        } catch (\Exception $e) {
+            return response()->json([
+                'NUMCODE' => -1,
+                'STRMESSAGE' => $e->getMessage(),
+                'RESPONSE' => [],
+                'SUCCESS' => false,
+            ], 401);
+        }
 
     }
 
-
-
-
-public function validatoken(String $token){
-    try {
-        $body = '{}';
-        $client = new Client();
-        $headers = [
-                   'Content-Type' => 'application/json',
-                   'Authorization'=> $token
-                   ];
-         $req = new Psr7Request('POST', env('APP_LOGIN_URL').'/api/verify', $headers, $body);
-         $res = $client->sendAsync($req)->wait();
-         //$data = json_decode($res->getBody()->getContents());
-         $data = $res->getStatusCode();
-         return $data;
+    public function validatoken(String $token)
+    {
+        try {
+            $body = '{}';
+            $client = new Client();
+            $headers = [
+                'Content-Type' => 'application/json',
+                'Authorization' => $token,
+            ];
+            $req = new Psr7Request('POST', env('APP_LOGIN_URL') . '/api/verify', $headers, $body);
+            $res = $client->sendAsync($req)->wait();
+            //$data = json_decode($res->getBody()->getContents());
+            $data = $res->getStatusCode();
+            return $data;
         } catch (\Exception $e) {
             return $e->getMessage();
         }
-}
+    }
 
 }
