0a7403071aeb57e766a987fe18e25bf328c623f7
aagarcia25 <109979605+aagarcia25@users.noreply.github.com>
2023-09-08 12:15:39 -0600

    ajustes
    ajustes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,6 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use App\Http\Controllers\FilesController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,28 +12,21 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
-
+ */
 
 Route::prefix('ApiDoc')->group(function () {
     //Prefijo ApiDoc, todo lo que este dentro de este grupo se accedera escribiendo ApiDoc en el navegador, es decir /api/ApiDoc/*
-    Route::group(['middleware' => ['jwt.verify']], function() {
+    Route::get('validacion', [FilesController::class, 'validacion']);
+
+    Route::group(['middleware' => ['jwt.verify']], function () {
         //Todo lo que este dentro de este grupo requiere verificación de usuario.
-        Route::post('ListFile',           [FilesController::class,'ListFile']);
-        Route::post('SaveFile',           [FilesController::class,'SaveFile']);
-        Route::post('GetByName',          [FilesController::class,'GetByName']);
-        Route::post('DeleteFile',         [FilesController::class,'DeleteFile']);
-        Route::post('DeleteFileByRoute',  [FilesController::class,'DeleteFileByRoute']);
-        Route::post('GetByRoute',         [FilesController::class,'GetByRoute']);
-        
+
+        Route::post('ListFile', [FilesController::class, 'ListFile']);
+        Route::post('SaveFile', [FilesController::class, 'SaveFile']);
+        Route::post('GetByName', [FilesController::class, 'GetByName']);
+        Route::post('DeleteFile', [FilesController::class, 'DeleteFile']);
+        Route::post('DeleteFileByRoute', [FilesController::class, 'DeleteFileByRoute']);
+        Route::post('GetByRoute', [FilesController::class, 'GetByRoute']);
+
     });
 });
-
-
-
-
-
-
-
-
-
