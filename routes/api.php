3ac7c81d062e99d82668e4f36d0575f530cb9211
Israelcacapmex <124214078+Israelcacapmex@users.noreply.github.com>
2024-08-20 10:56:15 -0600

    api ListFileUploadFile

--- routes/api.php
+++ routes/api.php
@@ -32,5 +32,6 @@ Route::prefix('ApiDoc')->group(function () {
         Route::post('moverArchivos', [FilesController::class, 'moverArchivos']);
         Route::post('VerificaMueveArchivos', [FilesController::class, 'VerificaMueveArchivos']);
         Route::post('ListFileFull', [FilesController::class, 'ListFileFull']);
+        Route::post('ListFileUploadFile', [FilesController::class, 'ListFileUploadFile']);
     });
 });
