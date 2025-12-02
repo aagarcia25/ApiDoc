25bcd942b5fc1c933b1b4844205c0bbe54470889
JonhyLaFarm <azael261188@hotmail.com>
2025-12-02 12:00:06 -0600

    Rutas

--- routes/api.php
+++ routes/api.php
@@ -32,12 +32,14 @@ Route::prefix('ApiDoc')->group(function () {
     Route::post('VerificaMueveArchivos', [FilesController::class, 'VerificaMueveArchivos']);
     Route::post('ListFileFull', [FilesController::class, 'ListFileFull']);
     Route::post('ListFileUploadFile', [FilesController::class, 'ListFileUploadFile']);
+    
+    Route::prefix('correo')->middleware('jwt.auth')->group(function () {
+        Route::get('/', [CorreoController::class, 'index']); // ejemplo: lista
+        Route::post('/envia-pass', [CorreoController::class, 'enviaPass']); // ejemplo: enviar correo
+        // agrega más endpoints aquí...
+    });
 
 });
 
 
-Route::prefix('correo')->middleware('jwt.auth')->group(function () {
-    Route::get('/', [CorreoController::class, 'index']); // ejemplo: lista
-    Route::post('/envia-pass', [CorreoController::class, 'enviaPass']); // ejemplo: enviar correo
-    // agrega más endpoints aquí...
-});
+
