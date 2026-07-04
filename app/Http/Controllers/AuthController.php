293216716f4ec9d3fdb9037e9875607a9cfe17f5
JonhyLaFarm <azael261188@hotmail.com>
2026-07-03 18:26:26 -0600

    Hash Auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Firebase\JWT\JWT;
 use App\ForoUsuario;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
